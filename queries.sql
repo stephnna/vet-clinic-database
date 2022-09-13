d3f95acad9f9a1c97e657d8fec4050e18cdbff88
stephnna <stephennnabuike@gmail.com>
2022-09-13 17:22:12 +0100

    Create animal table

--- queries.sql
+++ queries.sql
@@ -1,3 +1,13 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animal WHERE name LIKE '%mon%';
+SELECT * FROM animal WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT * FROM animal WHERE neutered=true AND escape_attempts<3;
+SELECT name, escape_attempts FROM animal WHERE weight_kg>10.5;
+SELECT date_of_birth FROM animal WHERE name='Agumon' OR name='Pikachu';
+SELECT * FROM animal WHERE neutered=true;
+SELECT * FROM animal WHERE name!= 'Gabumon';
+SELECT * FROM animal WHERE weight_kg >= 10.4 AND weight_kg <= 17.4;
+
+
+
--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
-);
+id INT PRIMARY KEY NOT NULL,
+name VARCHAR(250),
+date_of_birth DATE,
+excape_attempts INT,
+neutered BIT,
+weight_kg REAL);
