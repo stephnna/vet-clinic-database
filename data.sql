1419f5586ce21871fabb787d46b9192cf6496d30
stephnna <stephennnabuike@gmail.com>
2022-09-17 18:26:17 +0100

    Implement join table

--- data.sql
+++ data.sql
@@ -35,3 +35,40 @@ update animals set owner_id=2  where name='Gabumon' or name='Pikachu';
 update animals set owner_id=3  where name='devimon' or name='Plantmon';  
 update animals set owner_id=4  where name='Charmander' or name='Squirtle' or name='Blossom';
 update animals set owner_id=5  where name='Angemon' or name='Boarmon'; 
+
+
+insert into vets (name,age,date_of_graduation) values 
+('William Tatcher',45,'2000-04-23'),
+('Maisy Smith',26,'2019-01-17'),
+('Stephanie Mendez',64,'1981-05-04'),
+('Jack Harkness',38,'2008-06-08');
+
+insert into specializations (vet_id,species_id) values 
+(1,1),
+(3,1),
+(3,2),
+(4,2)
+;
+
+insert into visits (animal_id,vet_id,date_of_visit)
+ values
+ (1,1,'2020-04-24'),
+ (1,3,'2020-07-22'),
+ (2,4,'2021-02-02'),
+ (3,2,'2020-01-05'),
+ (3,2,'2020-03-08'),
+ (3,2,'2020-05-14'),
+ (4,3,'2021-05-04'),
+ (5,4,'2021-02-24'),
+ (6,2,'2019-12-21'),
+ (6,1,'2020-08-10'),
+ (6,2,'2021-04-07'),
+ (7,3,'2019-09-29'),
+ (11,4,'2020-10-03'),
+ (11,3,'2020-11-04'),
+ (8,2,'2019-01-24'),
+ (8,2,'2020-02-27'),
+ (8,2,'2020-08-03'),
+ (12,3,'2020-05-24'),
+ (12,1,'2021-11-01'),
+ (8,2,'2019-05-15');
--- queries.sql
+++ queries.sql
@@ -9,7 +9,6 @@ SELECT * FROM animal WHERE neutered=true;
 SELECT * FROM animal WHERE name!= 'Gabumon';
 SELECT * FROM animal WHERE weight_kg >= 10.4 AND weight_kg <= 17.4;
 
-
 /* Second day project */
 -- first transaction
 update animal
@@ -53,3 +52,54 @@ select full_name, count(name) from owners left join animals on animals.owner_id=
 select animals.name from owners join animals on animals.owner_id= owners.id where owners.full_name='Jennifer Orwell' and animals.species_id=2;
 select animals.name from owners join animals on animals.owner_id= owners.id where owners.full_name='Dean Winchester' and animals.escape_attempts=0;
 select owners.full_name, count(animal.name) from owners join animal on animal.owner_id = owners.id group by owners.full_name;
+
+
+/*Queries that provide answers to the questions from Fourth day tutorials.*/
+
+select animals.name, visits.date_of_visit,vets.name from vets join visits on vets.id=visits.vet_id join animals on animals.id=visits.animal_id where vets.name='William Tatcher' order by visits.date_of_visit desc limit 1;
+select count(animals.name),vets.name from vets join visits on vets.id=visits.vet_id join animals on animals.id=visits.animal_id where vets.name='Stephanie Mendez' group by vets.name;
+select vets.name ,species.name from vets join specializations on vets.id=specializations.vet_id join species on species.id = specializations.species_id;
+select animals.name from vets join visits on vets.id=visits.vet_id join animals on  animals.id=visits.animal_id where vets.name='Stephanie Mendez' and visits.date_of_visit between  '2020-04-1' and '2020-08-30';
+select count(animals.name),animals.name from vets join visits on vets.id=visits.vet_id join animals on animals.id=visits.animal_id group by animals.name order by count desc limit 1;
+select animals.name, visits.date_of_visit,vets.name from vets join visits on vets.id=visits.vet_id join animals on animals.id=visits.animal_id where vets.name='Maisy Smith' order by visits.date_of_visit desc limit 1;
+select count(animals.name),animals.name from vets join visits on vets.id=visits.vet_id join animals on animals.id=visits.animal_id group by animals.name order by count desc limit 1;
+
+select animals.name,animals.date_of_birth,
+escape_attempts,
+weight_kg,neutered, 
+vets.name ,
+vets.age,
+vets.date_of_graduation,
+ visits.date_of_visit 
+from vets 
+join visits on vets.id=visits.vet_id 
+join animals on animals.id=visits.animal_id 
+order by visits.date_of_visit desc limit 1;
+
+SELECT
+  COUNT(*)
+FROM
+  visits
+  RIGHT JOIN animals ON animals.id = visits.animal_id
+  RIGHT JOIN vets ON vets.id = visits.vet_id
+  LEFT JOIN specializations ON specializations.vet_id = vets.id
+  LEFT JOIN species ON specializations.species_id = species.id
+WHERE
+  specializations.species_id <> animals.species_id;
+
+  SELECT
+  species.name,
+  COUNT(*)
+FROM
+  visits
+  RIGHT JOIN animals ON animals.id = visits.animal_id
+  RIGHT JOIN vets ON vets.id = visits.vet_id
+  RIGHT JOIN species ON animals.species_id = species.id
+WHERE
+  vets.name = 'Maisy Smith'
+GROUP BY
+  species.name
+ORDER BY
+  count DESC
+LIMIT
+  1;
